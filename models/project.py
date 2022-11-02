5649719868ba7a917f21d099b8c2894835dc97bb
Carlitonchin <carlozalejandro98@gmail.com>
2022-11-02 19:44:15 -0300

    turn model project into a function that return a dictionary

--- models/project.py
+++ models/project.py
@@ -1,16 +1,11 @@
-class Project:
-    def __init__(self, row):
-        self.id = row[0]
-        self.project_images = row[1]
-        self.project_description = row[2]
-        self.project_name = row[3]
-        self.project_active = row[4]
+def create_project(row):
+    project = {
+        "id": row[0],
+    "project_images" : row[1],
+    "project_description" : row[2],
+    "project_name" : row[3],
+    "project_active" : row[4]
+    }
 
-    def to_json(self):
-        me = {}
-        me['id'] = self.id
-        me['project_images'] = self.project_images
-        me['project_description'] = self.project_description
-        me['project_name'] = self.project_name
-        me['project_active'] = self.project_active
-        return me
+    return project
+        
