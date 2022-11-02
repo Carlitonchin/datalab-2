1f8784aa8c62c50cfb03d88163d1373228536690
Carlitonchin <carlozalejandro98@gmail.com>
2022-11-02 19:23:23 -0300

    add to_json function to project model

--- models/project.py
+++ models/project.py
@@ -1,7 +1,16 @@
 class Project:
     def __init__(self, row):
         self.id = row[0]
-        self.project_images = row[1][0]
-        self.project_description = row[1][1]
-        self.project_name = row[1][2]
-        self.project_active = row[1][3]
+        self.project_images = row[1]
+        self.project_description = row[2]
+        self.project_name = row[3]
+        self.project_active = row[4]
+
+    def to_json(self):
+        me = {}
+        me['id'] = self.id
+        me['project_images'] = self.project_images
+        me['project_description'] = self.project_description
+        me['project_name'] = self.project_name
+        me['project_active'] = self.project_active
+        return me
