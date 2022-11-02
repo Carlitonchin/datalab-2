8d864a0b61871f4e26b6ebd5f35a14127987b644
Carlitonchin <carlozalejandro98@gmail.com>
2022-11-02 18:50:36 -0300

    create router with sqlite connection

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/proyecto/<id_proyecto>")
+def project(id_proyecto):
+    con = sqlite3.connect('sitio.db')
+   
+    
+    
+    con.close()
+    return "Hello World"
