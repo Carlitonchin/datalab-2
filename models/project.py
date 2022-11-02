576537965ac64918cb60b4da586207f465cddced
Carlitonchin <carlozalejandro98@gmail.com>
2022-11-02 18:54:37 -0300

    add project model

--- models/project.py
+++ models/project.py
@@ -1,3 +1,7 @@
 class Project:
     def __init__(self, row):
-        pass
+        self.id = row[0]
+        self.project_images = row[1][0]
+        self.project_description = row[1][1]
+        self.project_name = row[1][2]
+        self.project_active = row[1][3]
