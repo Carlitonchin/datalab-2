72af1ebcab15c9ae60c6598b65e0b0436d6f8810
Carlitonchin <carlozalejandro98@gmail.com>
2022-11-02 19:24:26 -0300

    call get_projects function and return results

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import encodings
+from encodings.utf_8 import encode
 from flask import Flask
 import sqlite3
+from .repo import get_projects_like
+import json
 
 app = Flask(__name__)
 
 @app.route("/proyecto/<id_proyecto>")
 def project(id_proyecto):
     con = sqlite3.connect('sitio.db')
-   
+    cur = con.cursor()
     
+    projects = get_projects_like(cur, id_proyecto)
+    result = json.dumps(projects, ensure_ascii=False)
     
     con.close()
-    return "Hello World"
+    return result
