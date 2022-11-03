1edc22666a2b02f6a529180e282571ad7a5b4a08
Carlitonchin <carlozalejandro98@gmail.com>
2022-11-02 21:09:33 -0300

    add user 'model'

---
+++ models/user.py
@@ -0,0 +1,22 @@
+def create_user(row):
+    user = {
+        "id" : row[0],
+        "username": row[1],
+        "password" : row[2],
+        "profile_picture" : row[3],
+        "user_full_name" : row[4],
+
+        "role" : {
+            "id":row[7],
+            "name":row[8],
+            "description":row[9]
+        }
+    }
+
+    try:
+        user["password"] = user["password"].decode("utf-8")
+    except AttributeError:
+        pass
+
+    return user
+    
